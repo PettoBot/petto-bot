196a243323908caa2cc7de2735d4fd11b78f5ce4
liamt8d <juliancamilparratrujillo@gmail.com>
2026-08-04 11:31:42 -0600

    Remove conflicting roleplay kick command

--- src/commands/roleplay/kick.js
+++
@@ -1,2 +0,0 @@
-const { createRoleplayCommand } = require('../../utils/roleplay');
-module.exports = createRoleplayCommand('kick', { label: 'kick', verb: 'gives a harmless roleplay kick to', self: '**{actor}** kicks the air dramatically.', description: 'Give someone a harmless roleplay kick.' });
